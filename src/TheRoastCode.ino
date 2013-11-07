d82dcb47559a6601af07447bfa7474794f4d7497
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-11-06 17:43:57 -0800

    Lots of things: expanded state machine; added hardware control functions

--- src/TheRoastCode.ino
+++ src/TheRoastCode.ino
@@ -17,6 +17,7 @@
   Maximum relay switches = 100 times per roast
 
   Temperature
+        |
   100% -|       ********%%%%%%%%%%%%%%%%%
         |      *        #
    75% -|     *         #
@@ -27,7 +28,7 @@
 	|*
    0%  ----------------------------------
         |       |       |       |       |
-        0      25      50      75      100
+   Time 0      25      50      75      100
  */
 
 #include "Adafruit_MAX31855.h"
@@ -36,26 +37,34 @@
 
 typedef enum ROAST_STATE {
   ROAST_IDLE,
+  ROAST_PREHEAT,
   ROAST_ROASTING,
-  ROAST_ERROR,
+  ROAST_COOLING,
 } roastState_t;
 
 typedef enum HEAT_STATE {
   HEAT_IDLE,
+  HEAT_PRE,
   HEAT_RAMP,
   HEAT_FULL,
 } heatState_t;
 
 typedef enum FAN_STATE {
   FAN_IDLE,
-  FAN_HALF,
+  FAN_PARTIAL,
   FAN_FULL,
 } fanState_t;
+
+typedef enum MOTOR_STATE {
+  MOTOR_ON,
+  MOTOR_OFF,
+} motorState_t;
 
 // states
 roastState_t roast_state = ROAST_IDLE;
 heatState_t heat_state = HEAT_IDLE;
 fanState_t fan_state = FAN_IDLE;
+motorState_t motor_state = MOTOR_OFF;
 
 // pins
 const int FAN_PIN = 9;  // PWM one of 3, 5, 6, 9, 10, or 11
@@ -68,18 +77,30 @@ const int THERMO_CS0 = 5;
 const int THERMO_CS1 = 6;
 
 // constants
-const int ROAST_TIME = 2;
+const int ROAST_TIME = 20;  // minutes
+const int RAMP_STEP_MICRO = 30000;  // ms, min = 12000
+const int SENSOR_SAMPLING_TIME = 1000;  // ms
+const int HEAT_FULL_TIME = 0.25 * ROAST_TIME;
+const int FAN_FULL_TIME = 0.5 * ROAST_TIME;
+const int FAN_PARTIAL_PERCENT = 50;
+const int FAN_FULL_PERCENT = 100;
+
+const int TEMP_COOL = 50;  // Especially arbitrary
+const int TEMP_READY = 100;
 const int TEMP_MAX = 250;
 const int TEMP_STEP = 5;
-const int RAMP_STEP_MICRO = 30;  // Min = 12
-const int SENSOR_SAMPLING_TIME = 1;
+
 const int MS_IN_MINUTE = 60000;
 
 // thermocouples
+const int INTERNAL_TEMP = 0;  // Which one is internal
 Adafruit_MAX31855 thermocouple0 (THERMO_CLK, THERMO_CS0, THERMO_DO);
 Adafruit_MAX31855 thermocouple1 (THERMO_CLK, THERMO_CS1, THERMO_DO);
 Adafruit_MAX31855 temps[] = {thermocouple0, thermocouple1};
 
+// variables
+unsigned long start_time = 0;
+unsigned long next_read = 0;
 
 // prototypes
 unsigned long minutes_to_ms(int minutes);
@@ -106,14 +127,66 @@ void set_heat(uint8_t state) {
   digitalWrite(HEAT_PIN, state);
 }
 
-void set_motor(uint8_t state) {
-  // accepts HIGH/LOW for ON/OFF
-  digitalWrite(MOTOR_PIN, state);
+void heat_idle() {
+  if (heat_state != HEAT_IDLE) {
+    heat_off();
+    heat_state = HEAT_IDLE;
+  }
+}
+
+void heat_off() {
+  set_heat(LOW);
+}
+
+void heat_on() {
+  if (heat_state != HEAT_IDLE) {
+    set_heat(HIGH);
+  }
 }
 
 void set_fan(int percent) {
   // PWM f ~= 490 Hz
   analogWrite(FAN_PIN, percent_to_duty(percent));
+}
+
+void fan_idle() {
+  if (fan_state != FAN_IDLE) {
+    set_fan(0);
+    fan_state = FAN_IDLE;
+  }
+}
+
+void fan_partial() {
+  if (fan_state != FAN_PARTIAL) {
+    set_fan(FAN_PARTIAL_PERCENT);
+    fan_state = FAN_PARTIAL;
+ }
+}
+
+void fan_full() {
+  if (fan_state != FAN_FULL) {
+    set_fan(FAN_FULL_PERCENT);
+    fan_state = FAN_FULL;
+  }
+}
+
+void set_motor(uint8_t state) {
+  // accepts HIGH/LOW for ON/OFF
+  digitalWrite(MOTOR_PIN, state);
+}
+
+void motor_off() {
+  if (motor_state != MOTOR_OFF) {
+    set_motor(LOW);
+    motor_state = MOTOR_OFF;
+  }
+}
+
+void motor_on() {
+  if (motor_state != MOTOR_ON) {
+    set_motor(HIGH);
+    motor_state == MOTOR_ON;
+  }
 }
 
 double get_temp(int i) {
@@ -123,7 +196,7 @@ double get_temp(int i) {
     Serial.println(i);
     Serial.println("\n");
     Serial.println("Shutting down\n");
-    roast_state = ROAST_ERROR;
+    roast_state = ROAST_IDLE;
     return 0;
   } else {
     Serial.println("Temp ");
@@ -137,12 +210,6 @@ double get_temp(int i) {
   }
 }
 
-double get_avg_temp() {
-  double temp = 0;
-  for (int i = 0; i < 2; i++) temp += get_temp(i);
-  return temp/2;
-}
-
 // Define set-up loop
 void setup() {
   Serial.begin(9600);
@@ -150,14 +217,98 @@ void setup() {
   pinMode(HEAT_PIN, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
 
-  set_heat(LOW);
-  set_fan(LOW);
-  set_motor(LOW);
-  
   delay(500);
+
+  start_time = millis();
+  next_read = start_time;
 } 
 
 // Main loop
 void loop() {
-  unsigned long start_time;
+  unsigned long elapsed_time = millis() - start_time;
+  double internal_temp = 0;
+
+  if (elapsed_time > next_read) {
+    next_read += SENSOR_SAMPLING_TIME;
+    internal_temp = get_temp(INTERNAL_TEMP);
+  }
+
+  switch (roast_state) {
+  case ROAST_IDLE:
+    heat_idle();
+    fan_idle();
+    motor_off();
+    // Indicate cooldown is done
+    break;
+  case ROAST_PREHEAT:
+    motor_on();
+    heat_state = HEAT_PRE;
+  case ROAST_ROASTING:
+    motor_on();
+    if (fan_state == FAN_IDLE) {
+      fan_partial();
+    }
+    if (elapsed_time > minutes_to_ms(ROAST_TIME)) {
+      roast_state = ROAST_COOLING;
+    }
+    break;
+  case ROAST_COOLING:
+    // Turn off heat, max fans until cool
+    heat_idle();
+    fan_full();
+    if (internal_temp < TEMP_COOL) {
+      roast_state = ROAST_IDLE;
+    }
+    break;
+  }
+
+  switch (heat_state) {
+  case HEAT_IDLE:
+    break;
+  case HEAT_PRE:
+    // Get temperature to TEMP_READY
+    heat_on();
+    if (internal_temp > TEMP_READY) {
+      start_time = elapsed_time;  // Restart timer
+      roast_state = ROAST_ROASTING;
+      heat_state = HEAT_RAMP;
+      // Indicate pre-heating is done
+    }
+    break;
+  case HEAT_RAMP:
+    // Gradually increase temperature to TEMP_MAX
+    if ((elapsed_time > HEAT_FULL_TIME) && (internal_temp > TEMP_MAX)) {
+      heat_state = HEAT_FULL;
+    }
+    break;
+  case HEAT_FULL:
+    // Maintain temperature at TEMP_MAX
+    // CAUTION: code needs to be implemented to switch slowly
+    if (internal_temp > TEMP_MAX) {
+      heat_off();
+    } else if (internal_temp < (TEMP_MAX - TEMP_STEP)) {
+      heat_on;
+    }
+    break;
+  }
+
+  switch (fan_state) {
+  case FAN_IDLE:
+    break;
+  case FAN_PARTIAL:
+    if (elapsed_time > minutes_to_ms(FAN_FULL_TIME)) {
+      fan_full();
+    }
+    break;
+  case FAN_FULL:
+    break;
+  }
+
+  switch (motor_state) {
+  case MOTOR_OFF:
+    break;
+  case MOTOR_ON:
+    break;
+  }
+
 }
